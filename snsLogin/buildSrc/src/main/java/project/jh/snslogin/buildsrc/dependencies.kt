047e73620c80433255aa47a840bb165c39f1f808
jihye-Noh <jihye7369@gmail.com>
2022-06-12 23:18:09 +0900

    creating buildSrc folder

--- snsLogin/buildSrc/src/main/java/project/jh/snslogin/buildsrc/dependencies.kt
+++ snsLogin/buildSrc/src/main/java/project/jh/snslogin/buildsrc/dependencies.kt
@@ -5,3 +5,23 @@ package project.jh.snslogin.buildsrc
  * Date: 2022/05/29
  */
 
+object Libs {
+    object Google {
+        const val material = "com.google.android.material:material:1.6.1"
+    }
+
+    object AndroidX {
+        const val coreKtx = "androidx.core:core-ktx:1.8.0"
+        const val appcompat = "androidx.appcompat:appcompat:1.4.2"
+        const val constraintlayout = "androidx.constraintlayout:constraintlayout:2.1.4"
+        const val navigationFragmentKtx = "androidx.navigation:navigation-fragment-ktx:2.4.2"
+        const val navigationUiKtx = "androidx.navigation:navigation-ui-ktx:2.4.2"
+
+        const val junit = "androidx.test.ext:junit:1.1.3"
+        const val espressoCore = "androidx.test.espresso:espresso-core:3.4.0"
+    }
+
+    object Test {
+        const val junit = "junit:junit:4.13.2"
+    }
+}
