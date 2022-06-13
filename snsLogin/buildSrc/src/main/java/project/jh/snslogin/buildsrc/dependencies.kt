6d4159572ac8b3e6107d72268582c8ae86a04c2d
jihye-Noh <jihye7369@gmail.com>
2022-06-13 11:05:24 +0900

    adding compose library

--- snsLogin/buildSrc/src/main/java/project/jh/snslogin/buildsrc/dependencies.kt
+++ snsLogin/buildSrc/src/main/java/project/jh/snslogin/buildsrc/dependencies.kt
@@ -6,6 +6,10 @@ package project.jh.snslogin.buildsrc
  */
 
 object Libs {
+    object Versions {
+        const val compose = "1.1.1"
+    }
+
     object Google {
         const val material = "com.google.android.material:material:1.6.1"
     }
@@ -16,9 +20,25 @@ object Libs {
         const val constraintlayout = "androidx.constraintlayout:constraintlayout:2.1.4"
         const val navigationFragmentKtx = "androidx.navigation:navigation-fragment-ktx:2.4.2"
         const val navigationUiKtx = "androidx.navigation:navigation-ui-ktx:2.4.2"
+        const val startupRuntime = "androidx.startup:startup-runtime:1.1.1"
 
-        const val junit = "androidx.test.ext:junit:1.1.3"
-        const val espressoCore = "androidx.test.espresso:espresso-core:3.4.0"
+        object Compose {
+            const val ui = "'androidx.compose.ui:ui:${Versions.compose}"
+            const val uiTooling = "androidx.compose.ui:ui-tooling:${Versions.compose}"
+            const val foundation = "androidx.compose.foundation:foundation:${Versions.compose}"
+            const val material = "androidx.compose.material:material:${Versions.compose}"
+            const val materialIconsCore = "androidx.compose.material:material-icons-core:${Versions.compose}"
+            const val materialIconsExtended = "androidx.compose.material:material-icons-extended:${Versions.compose}"
+            const val activityCompose = "androidx.activity:activity-compose:1.4.0"
+            const val lifecycleViewmodelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1"
+            const val runtimeLivedata = "androidx.compose.runtime:runtime-livedata:${Versions.compose}"
+            const val runtimeRxjava2 = "androidx.compose.runtime:runtime-rxjava2:${Versions.compose}"
+        }
+
+        object Test {
+            const val junit = "androidx.test.ext:junit:1.1.3"
+            const val espressoCore = "androidx.test.espresso:espresso-core:3.4.0"
+        }
     }
 
     object Test {
