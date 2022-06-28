bb08b25b1d1a0357e2e2fd44cbc56828cc1daf71
jihye-Noh <jihye7369@gmail.com>
2022-06-28 21:09:15 +0900

    adds lint.xml

--- snsLogin/buildSrc/src/main/java/project/jh/snslogin/buildsrc/VersionExtensions.kt
+++ snsLogin/buildSrc/src/main/java/project/jh/snslogin/buildsrc/VersionExtensions.kt
@@ -1,9 +1,20 @@
 import org.gradle.api.Project
 
 fun Project.buildVersionCode(): Int {
+    val  s = file("version.properties")
     return 0
 }
 
 fun Project.buildVersionName(): String {
     return ""
+}
+
+private fun buildNumber(): Int {
+    val buildNumber = System.getenv("BITRISE_BUILD_NUMBER")?.toInt() ?: 0
+
+    if(buildNumber > 9999) {
+        throw IllegalArgumentException("build number must not be greater than 9999")
+    }
+
+    return buildNumber
 }
